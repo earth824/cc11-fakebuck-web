a220f4dfaa92d4969760696df1a7d4ea40c73b96
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-26 12:58:21 +0700

    add auth context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import AuthContextProvider from './contexts/AuthContext';
 import Router from './route/Router';
 
 function App() {
-  return <Router />;
+  return (
+    <AuthContextProvider>
+      <Router />
+    </AuthContextProvider>
+  );
 }
 
 export default App;
