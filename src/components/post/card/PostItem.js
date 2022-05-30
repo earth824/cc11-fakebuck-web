b923aeeea6e7780d106f4972f19d54c025dbe149
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-30 17:55:02 +0700

    add post context

---
+++ src/components/post/card/PostItem.js
@@ -0,0 +1,13 @@
+import PostBody from './body/PostBody';
+import PostHeader from './header/PostHeader';
+
+function PostItem() {
+  return (
+    <div className="border bg-white rounded-lg shadow-sm px-3 tw-pt-3">
+      <PostHeader />
+      <PostBody />
+    </div>
+  );
+}
+
+export default PostItem;
